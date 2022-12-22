2a82b018448d553a47e638b2546e6be638ed3761
Lucas Nascimento Silva <turano1995@hotmail.com>
2022-12-22 16:35:35 -0300

    fazendo pequenos ajustes, instruido pelo nosso orientador.

--- client/src/pages/home/sections/SectionPresentation.jsx
+++ client/src/pages/home/sections/SectionPresentation.jsx
@@ -12,7 +12,9 @@ function SectionPresentation(){
                 <h1>
                     <img src={Banner} alt="banner"/>                 
                 </h1>
-                <p>O melhor serviço para você!</p>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+
+The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
                 <ButtonLink to="/" className="btn-link-1">Saiba Mais!</ButtonLink>
             </div>
             <MediumWrap className="images">
